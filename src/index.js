8be551fc0e4e721e2019b3359fee07ea910083b9
don <tanvirrashiddon@gmail.com>
2018-09-16 01:29:02 +0600

    Box added

--- src/index.js
+++ src/index.js
@@ -2,37 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Box extends React.Component {
+	selectBox = () => {
+		this.props.selectBox(this.props.row, this.props.col);
+	}
+
+	render() {
+		return (
+			<div
+				className={this.props.boxClass}
+				id={this.props.id}
+				onClick={this.selectBox}
+			/>
+		);
+	}
+}
+
 class Grid extends React.Component {
 	render() {
-		const width = this.props.cols * 14;
-
+		const width = (this.props.cols * 14);
 		var rowsArr = [];
 
 		var boxClass = "";
-		for (let i = 0; i < this.props.rows; i++) {
-			for (let j = 0; j < this.props.cols; j++) {
-				let boxId = i + "_" +j;
+		for (var i = 0; i < this.props.rows; i++) {
+			for (var j = 0; j < this.props.cols; j++) {
+				let boxId = i + "_" + j;
 
 				boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
 				rowsArr.push(
 					<Box
-						boxClass={this.boxClass}
+						boxClass={boxClass}
 						key={boxId}
 						boxId={boxId}
 						row={i}
 						col={j}
 						selectBox={this.props.selectBox}
 					/>
-				)
+				);
 			}
 		}
+
 		return (
 			<div className="grid" style={{width: width}}>
 				{rowsArr}
 			</div>
-		)
+		);
 	}
 }
+
 
 class Main extends React.Component {
 
@@ -49,18 +66,18 @@ class Main extends React.Component {
 	}
 
   render(){
-    return (
-      <div>
-        <h1>The Game of Life</h1>
-				<Grid 
-					gridFull={this.gridFull}
+	return (
+	  <div>
+		<h1>The Game of Life</h1>
+        <Grid
+					gridFull={this.state.gridFull}
 					rows={this.rows}
 					cols={this.cols}
 					selectBox={this.selectBox}
 				/>
 				<h2>Generation: {this.state.generation}</h2>
-      </div>
-    )
+	  </div>
+	)
   }
 }
 
